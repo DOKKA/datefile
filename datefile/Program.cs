e5778a3bd2935c65147054dba6fff36da1aa2782
dokka <scuffmark@gmail.com>
2015-05-11 18:29:11 -0500

    initial commit

---
+++ datefile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datefile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "datefile.txt");
+            System.IO.File.AppendAllText(path,DateTime.Now.ToString()+"\r\n");
+        }
+    }
+}
